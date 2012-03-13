58ceb0438d6ff46ceceea21b5a0f315a2ce1c13c
aahomchenko@gmail.com <aahomchenko@gmail.com@a2931c39-fb69-76df-b261-3097618979e8>
2012-03-13 17:19:31 +0000

    add without credentials

---
+++ src/myline/server/security/SecurityConstants.java
@@ -0,0 +1,27 @@
+package myline.server.security;
+
+public class SecurityConstants {
+	//security
+	
+	//PRODUCT
+	public static final String HASH_PROD = "";
+	public static final String KEY_PROD = "";
+	
+	//DEBUG
+	public static final String HASH_TEST = "";
+	public static final String KEY_TEST = "";
+	
+	public static final String API_ID = "";
+	public static final String API_SECRET = "";
+	
+	//bit.ly
+	public static final String SHORT_API_LOGIN = "";
+	public static final String SHORT_API_KEY = "";
+	
+	//nonsecurity
+	public static final String NONPAS_USER = "";
+	public static final String NONPAS_SECRET_HASH = "";
+	
+	public static final String NONPAS_USER2 = "";
+	public static final String NONPAS_SECRET_HASH2 = "";
+}
