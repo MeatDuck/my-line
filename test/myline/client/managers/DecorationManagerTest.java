1835883ba795ee75ee3df12c11f1309d1d91a99f
aahomchenko@gmail.com <aahomchenko@gmail.com@a2931c39-fb69-76df-b261-3097618979e8>
2012-04-29 14:43:02 +0000

    junit test support added

---
+++ test/myline/client/managers/DecorationManagerTest.java
@@ -0,0 +1,93 @@
+package myline.client.managers;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.google.gwt.junit.client.GWTTestCase;
+
+/**
+ * @author AND
+ *
+ */
+public class DecorationManagerTest extends GWTTestCase {
+
+	/**
+	 * Test method for {@link myline.client.managers.DecorationManager#getInstance()}.
+	 */
+	@Test
+	public void testGetInstance() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link myline.client.managers.DecorationManager#showLogin()}.
+	 */
+	@Test
+	public void testShowLogin() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link myline.client.managers.DecorationManager#showMain()}.
+	 */
+	@Test
+	public void testShowMain() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link myline.client.managers.DecorationManager#showLoading()}.
+	 */
+	@Test
+	public void testShowLoading() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link myline.client.managers.DecorationManager#stopLoading(com.google.gwt.user.client.ui.Panel)}.
+	 */
+	@Test
+	public void testStopLoading() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link myline.client.managers.DecorationManager#fixSize()}.
+	 */
+	@Test
+	public void testFixSize() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link myline.client.managers.DecorationManager#showError(java.lang.Throwable)}.
+	 */
+	@Test
+	public void testShowError() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link myline.client.managers.DecorationManager#refresh()}.
+	 */
+	@Test
+	public void testRefresh() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link myline.client.managers.DecorationManager#refresh(int)}.
+	 */
+	@Test
+	public void testRefreshInt() {
+		fail("Not yet implemented");
+	}
+
+	@Override
+	public String getModuleName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
